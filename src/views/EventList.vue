f3b00d453c1770a6a238eead552ae888f94c0b5a
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-03 19:34:54 -0700

    Refactor EventList.vue

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -6,7 +6,9 @@
 
 <script>
 // @ is an alias to /src
+/* eslint-disable prettier/prettier */
 import EventCard from "@/components/EventCard.vue";
+import EventService from "@/services/EventService.js";
 
 export default {
   name: "EventList",
@@ -15,42 +17,14 @@ export default {
   },
   data() {
     return {
-      events: [
-        {
-          id: 5928101,
-          category: "Animal Welfare",
-          title: "Cat Adoption Day",
-          description: "Find your new feline friend at this event.",
-          location: "Meow Town",
-          date: "January 28, 2022",
-          time: "12:00",
-          petsAllowed: true,
-          organizer: "Kat Laydee",
-        },
-        {
-          id: 4582797,
-          category: "Food",
-          title: "Community Gardening",
-          description: "Join us as we tend to the community edible plants.",
-          location: "Flora City",
-          date: "March 14, 2022",
-          time: "10:00",
-          petsAllowed: true,
-          organizer: "Fern Pollin",
-        },
-        {
-          id: 8419988,
-          category: "Sustainability",
-          title: "Beach Cleanup",
-          description: "Help pick up trash along the shore.",
-          location: "Playa Del Carmen",
-          date: "July 22, 2022",
-          time: "11:00",
-          petsAllowed: false,
-          organizer: "Carey Wales",
-        },
-      ],
+      events: null,
     };
+  },
+  created() {
+    EventService.getEvents()
+      .then( response => { 
+        this.events = response.data 
+      });
   },
 };
 </script>
