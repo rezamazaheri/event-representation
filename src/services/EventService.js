a64a992935d95c01612aeb70b18d6d4251be21f5
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-04 21:20:57 -0700

    Make getEvent() API Call

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -13,5 +13,8 @@ const apiClient = axios.create({
 export default {
   getEvents() {
     return apiClient.get("/events");
+  },
+  getEvent(id) {
+    return apiClient.get("/events/" + id);
   }
 }
