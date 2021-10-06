3e31d982c2b41cb444d31deff3e6d6c0450ae20f
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-05 23:27:25 -0700

    Use Props Function Mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { createRouter, createWebHistory } from "vue-router";
 import EventList from "../views/EventList.vue";
 import EventDetails from "../views/EventDetails.vue";
@@ -8,6 +9,7 @@ const routes = [
     path: "/",
     name: "EventList",
     component: EventList,
+    props: route => ({ page: parseInt(route.query.page) || 1}),
   },
   {
     path: "/event/:id",
