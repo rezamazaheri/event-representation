393814ce674d0de45f6402bba9e9fe759ea72c68
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-05 23:33:09 -0700

    Modify EventList.vue to Pass on 'perPage' and 'page'

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="events">
+    <h1>Events For Good</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1">&#60; Prev Page</router-link>
+
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage">Next Page &#62;</router-link>
+    </div>
   </div>
 </template>
 
@@ -9,23 +23,40 @@
 /* eslint-disable prettier/prettier */
 import EventCard from "@/components/EventCard.vue";
 import EventService from "@/services/EventService.js";
+import { watchEffect } from "vue";
 
 export default {
   name: "EventList",
+  props: {
+    page: {
+      type: Number
+    }
+  },
   components: {
     EventCard,
   },
   data() {
     return {
       events: null,
+      totalEvents: 0,
     };
   },
   created() {
-    EventService.getEvents()
-      .then( response => { 
-        this.events = response.data 
-      });
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(3, this.page)
+        .then( response => { 
+          this.events = response.data,
+          this.totalEvents = response.headers["x-total-count"]
+        });
+    })
   },
+  computed: {
+    hasNextPage() {
+      let totalPages = Math.ceil(this.totalEvents / 3);
+      return this.page < totalPages;
+    }
+  }
 };
 </script>
 
@@ -34,5 +65,20 @@ export default {
   display: flex;
   flex-direction: column;
   align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
 }
 </style>
