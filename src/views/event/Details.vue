09997a6ea48e861b00b41e60ec6c7d6acc6c5aa3
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-06 13:13:50 -0700

    Using Layout.vue to Have Less Code, Using Child Components: Details, Edit and Register

---
+++ src/views/event/Details.vue
@@ -0,0 +1,20 @@
+<template>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
+</template>
+
+<script>
+/* eslint-disable prettier/prettier */
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/event/Edit.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Edit the event here.</p>
+</template>
+
+<script>
+/* eslint-disable prettier/prettier */
+export default {
+  props: {
+    event: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
