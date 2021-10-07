a5c9eb5b4db64a3a6882e39773270285b61a63b1
Reza Mazaheri <rezamazaheri.email@gmail.com>
2021-10-07 14:53:43 -0700

    Use Programmatic Navigation in Created Hook

--- src/views/event/Layout.vue
+++ src/views/event/Layout.vue
@@ -31,7 +31,19 @@ export default {
     EventService.getEvent(this.id)
       .then( response => { 
         this.event = response.data 
-      });
+      })
+      .catch(error => {
+        if(error.response && error.response.status == 404) {
+          this.$router.push({
+          name: "404Resource",
+          params: {
+            resource: "event"
+          }
+          })
+        } else {
+          this.$router.push({ name: "NetworkError"});
+        }
+      })
   }
 }
 </script>
